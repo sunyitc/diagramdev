9cd01ebb0b64600cbdd1a9e8212476447f414dee
yisun <yisun@LAPTOP-RVO35NVV.lan>
2018-02-08 22:28:50 +0800

    1111

---
+++ TestGit/src/main/java/com/mvn1/TestGit/App.java
@@ -0,0 +1,13 @@
+package com.mvn1.TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestGit/src/main/java/com/mvn1/TestGit/T11111.java
@@ -0,0 +1,10 @@
+package com.mvn1.TestGit;
+
+public class T11111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
