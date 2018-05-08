583e0c059a2d3b1cc7c7df7c929d55b112a1e8c7
yisun <yisun@192.168.33.1>
2018-05-08 12:06:35 +0800

    cc

---
+++ SC_Master/SC_S_Config_Server_Git/src/main/java/org/SC_S_Config_Server_Git/ConfigServerGitApp.java
@@ -0,0 +1,13 @@
+package org.SC_S_Config_Server_Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConfigServerGitApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
