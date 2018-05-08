cf238ff440859c1a141b4dcfe52e2ee45ffcf3c9
yisun <yisun@192.168.33.1>
2018-05-09 07:09:29 +0800

    eeeee

--- SC_Master/SC_S_Client/src/main/java/org/SC_S_Client/App.java
+++ SC_Master/SC_S_Client/src/main/java/org/SC_S_Client/App.java
@@ -4,7 +4,9 @@ import org.SC_S_Common.Abc;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class App 
 {
     public static void main( String[] args )
@@ -49,7 +52,7 @@ public class App
     
 	@Value("${foo}")
 	String foo;
-	@RequestMapping(value = "/test")
+	@GetMapping(value = "/test")
 	public String test(){
 		return foo;
 	}
--- SC_Master/SC_S_Config_Server_Git/src/main/java/org/SC_S_Config_Server_Git/ConfigServerGitApp.java
+++ SC_Master/SC_S_Config_Server_Git/src/main/java/org/SC_S_Config_Server_Git/ConfigServerGitApp.java
@@ -4,7 +4,9 @@ package org.SC_S_Config_Server_Git;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerGitApp 
